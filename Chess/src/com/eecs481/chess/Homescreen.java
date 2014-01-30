6a2b95f306d6ceba026f056bd9e5bf05e3f49619
Jacob Korona <korona.j@gmail.com>
2014-01-30 16:29:40 -0500

    Committing what was missed in the previous commit

--- Chess/src/com/eecs481/chess/Homescreen.java
+++ Chess/src/com/eecs481/chess/Homescreen.java
@@ -3,25 +3,40 @@ package com.eecs481.chess;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-
-import com.parse.Parse;
+import android.view.MenuItem;
+import com.parse.ParseUser;
 
 public class Homescreen extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_homescreen);
-        Parse.initialize(this, "4SV3X5Flt3tqhr87pM29xI36jKYtUWnZWBBI70iH", "vxkrbGSjbstIO1UjUg6PGicEUCSvcEVo9p5kjgZ4");
-        
-    }
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_homescreen);
+
+      m_activityContext = this;
+   }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.homescreen, menu);
-        return true;
-    }
-    
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu) {
+      // Inflate the menu; this adds items to the action bar if it is present.
+      getMenuInflater().inflate(R.menu.homescreen_menu, menu);
+      return true;
+   }
+
+   @Override
+   public boolean onOptionsItemSelected(MenuItem item) {
+      switch (item.getItemId()) {
+         case R.id.action_settings:
+            return true;
+         case R.id.action_logout:
+            ParseUser.logOut();
+            m_activityContext.finish();
+            return true;
+         default:
+            return super.onOptionsItemSelected(item);
+      }
+   }
+
+   private Activity m_activityContext;
 }
