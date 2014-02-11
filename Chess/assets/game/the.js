381a77af2465cad602f06054dc6bb047d627206b
Evan Hahn <me@evanhahn.com>
2014-02-11 10:17:19 -0500

    begin to glue Java to JavaScript

--- Chess/assets/game/the.js
+++ Chess/assets/game/the.js
@@ -4,7 +4,7 @@ var boardWidth = Math.min($(window).width(), $(window).height());
 $('#board').css('width', boardWidth);
 
 var board = new ChessBoard('board', {
-	position: 'start',
+	position: ferry.getBoardState(),
 	showNotation: false,
 	pieceTheme: 'vendor/chesspieces/{piece}.png',
 	onChange: highlightLegalSquares
