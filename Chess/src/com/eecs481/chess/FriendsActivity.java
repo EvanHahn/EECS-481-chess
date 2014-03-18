de8794cc86615cdac872f74e7f4867a86170d667
Jake Korona <korona.j@gmail.com>
2014-03-18 11:07:11 -0400

    cleanup and doc FriendsActivity

--- Chess/src/com/eecs481/chess/FriendsActivity.java
+++ Chess/src/com/eecs481/chess/FriendsActivity.java
@@ -3,7 +3,6 @@ package com.eecs481.chess;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,7 +14,6 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 import ask.scanninglibrary.ASKActivity;
-
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
@@ -23,14 +21,14 @@ import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 /**
- * 
+ * The friends list activity.
  * 
  * @author Jake Korona
  */
 public class FriendsActivity extends ASKActivity {
 
    //////////////////////////////////////////////////////////////////////////
-   // Public methods
+   // Activity overrides
    //////////////////////////////////////////////////////////////////////////
 
    @Override
@@ -40,7 +38,7 @@ public class FriendsActivity extends ASKActivity {
 
       m_activityContext = this;
       m_user = ParseUser.getCurrentUser();
-      
+
       mSearchBox = (TextView) findViewById(R.id.searchBox);
       mSearchBox.setText("");
 
@@ -51,7 +49,7 @@ public class FriendsActivity extends ASKActivity {
             final String searchName = mSearchBox.getText().toString();
             if (searchName.isEmpty())
                return;
-            
+
             mSearchBox.setText("");
             searchForUser(searchName);
 
@@ -60,89 +58,117 @@ public class FriendsActivity extends ASKActivity {
       });
 
       ListView friendsListView = (ListView) findViewById(R.id.friendsListView);
+
       mAdapter = new FriendsListAdapter(m_activityContext, friendsListView);
-
       mAdapter.setList(m_friends);
-
       friendsListView.setAdapter(mAdapter);
-      
+
       refreshDisplayedFriends();
 
       friendsListView.setOnItemClickListener(new OnItemClickListener() {
          @Override
-         public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {        	 
-		    
-		    String opponent = (String)arg0.getItemAtPosition(position);
-		    ParseObject game = makeNewGame(opponent);
-		    
-		    ArrayList<String> gameParams = Utility.getGameParams(game);
-		    
-		    Intent intent = new Intent(m_activityContext, GameActivity.class);		    		    
-		    intent.putExtra(Consts.GAME_PARAMS, gameParams);
+         public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+
+            String opponent = (String)arg0.getItemAtPosition(position);
+            if (opponent.isEmpty() || opponent == null)
+               return;
+
+            ParseObject game = makeNewGame(opponent);
+            ArrayList<String> gameParams = Utility.getGameParams(game);
+
+            Intent intent = new Intent(m_activityContext, GameActivity.class);
+            intent.putExtra(Consts.GAME_PARAMS, gameParams);
 
             startActivity(intent);
          }
       });
-
    }
 
    //////////////////////////////////////////////////////////////////////////
    // Non-public methods
    //////////////////////////////////////////////////////////////////////////
-   
+
+   /**
+    * Constructs and saves a new game.
+    * 
+    * @param opponent the opponent's username
+    * @return a new game
+    */
    private ParseObject makeNewGame(final String opponent) {
-	    ParseObject game = new ParseObject(Consts.GAME_OBJECT);
-	    game.put(Consts.P1_FIELD, m_user.getUsername());
-	    game.put(Consts.P2_FIELD, opponent);
-	    game.put(Consts.STATUS_FIELD, m_user.getUsername());
-	    game.put(Consts.CUR_GAME_FIELD, Consts.NEW_BOARD);
-	    game.saveInBackground();
-	    
-	    return game;
-   }
-   
-   private void searchForUser(final String username) {
-	   if (m_friends.contains(username)) {
-		   makeToast("That person is already your friend!");
-		   return;
-	   }
-	   
-	   ParseQuery<ParseUser> query = ParseUser.getQuery();
-	   query.whereEqualTo("username", username);
-	   query.findInBackground(new FindCallback<ParseUser>() {
-	     public void done(List<ParseUser> objects, ParseException e) {
-	       if (e == null) {
-	           if (!objects.isEmpty()) {
-	        	   addFriend(objects.get(0).getUsername());
-	        	   refreshDisplayedFriends();
-	        	   makeToast("New friend " + objects.get(0).getUsername() + " added!");
-	           } else {
-	        	   makeToast("No user of that name!");
-	           }
-	       } else {
-	           makeToast("Something went wrong!");
-	       }
-	     }
-	   });
-   }
-   
-   private void refreshDisplayedFriends() {
-	    List<String> friends = m_user.getList(Consts.USER_FRIENDS);
-	    m_friends.clear();
-	    m_friends.addAll(friends);
-   		mAdapter.clear();
-   		mAdapter.setList(m_friends);
-   		mAdapter.notifyDataSetChanged();
+      ParseObject game = new ParseObject(Consts.GAME_OBJECT);
+      game.put(Consts.P1_FIELD, m_user.getUsername());
+      game.put(Consts.P2_FIELD, opponent);
+      game.put(Consts.STATUS_FIELD, m_user.getUsername());
+      game.put(Consts.CUR_GAME_FIELD, Consts.NEW_BOARD);
+      game.saveInBackground();
+
+      return game;
    }
 
-   private void addFriend(String name) {
-		ParseUser user = m_user;
-		user.add(Consts.USER_FRIENDS, name);
-		user.saveInBackground();
+   /**
+    * Searches Parse for a user.
+    * 
+    * @param username the username
+    */
+   private void searchForUser(final String username) {
+      if (username.isEmpty() || username == null)
+         return;
+
+      if (m_friends.contains(username)) {
+         makeToast("That person is already your friend!");
+         return;
+      }
+
+      ParseQuery<ParseUser> query = ParseUser.getQuery();
+      query.whereEqualTo("username", username);
+      query.findInBackground(new FindCallback<ParseUser>() {
+         @Override
+         public void done(List<ParseUser> objects, ParseException e) {
+            if (e == null) {
+               if (!objects.isEmpty()) {
+                  addFriend(objects.get(0).getUsername());
+                  refreshDisplayedFriends();
+                  makeToast("New friend " + objects.get(0).getUsername() + " added!");
+               }
+               else
+                  makeToast("No user of that name!");
+            }
+            else
+               makeToast("Something went wrong!");
+         }
+      });
    }
-   
+
+   /**
+    * Refreshes the list of friends.
+    */
+   private void refreshDisplayedFriends() {
+      List<String> friends = m_user.getList(Consts.USER_FRIENDS);
+      m_friends.clear();
+      m_friends.addAll(friends);
+      mAdapter.clear();
+      mAdapter.setList(m_friends);
+      mAdapter.notifyDataSetChanged();
+   }
+
+   /**
+    * Adds a user to the current user's friends list.
+    * 
+    * @param name the friend's username
+    */
+   private void addFriend(String name) {
+      ParseUser user = m_user;
+      user.add(Consts.USER_FRIENDS, name);
+      user.saveInBackground();
+   }
+
+   /**
+    * Constructs a short toast message.
+    * 
+    * @param message the message
+    */
    private void makeToast(String message) {
-	    Toast.makeText(m_activityContext, message, Toast.LENGTH_SHORT).show();
+      Toast.makeText(m_activityContext, message, Toast.LENGTH_SHORT).show();
    }
 
    //////////////////////////////////////////////////////////////////////////
@@ -153,11 +179,14 @@ public class FriendsActivity extends ASKActivity {
    private ASKActivity m_activityContext;
 
    /** The list of the current user's friends. */
-   private List<String> m_friends = Collections.synchronizedList(new ArrayList<String>());
-   
+   private final List<String> m_friends = Collections.synchronizedList(new ArrayList<String>());
+
+   /** The current user. */
    private ParseUser m_user;
-   
+
+   /** The adapter for the list of friends. */
    private FriendsListAdapter mAdapter;
-   
+
+   /** The search box. */
    private TextView mSearchBox;
 }
